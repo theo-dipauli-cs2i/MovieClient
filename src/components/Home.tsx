191c05f33c16a9f29934777f978f4a724cbfe170
theod <theod@PHOQUEYOU>
2025-05-21 16:43:54 +0200

    fix images

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,6 @@
 import { Navbar } from "./Navbar"
+import preactLogo from '../assets/preact.svg';
+import viteLogo from '../assets/vite.svg';
 
 export function Home() {
     return (
@@ -8,14 +10,14 @@ export function Home() {
                 <Navbar />
                 <div class="row">
                     <div class="col-3 d-flex justify-content-center">
-                        <img class="img-fluid w-75" src="/preact.svg" alt="" />
+                        <img class="img-fluid w-75" src={preactLogo} alt="" />
                     </div>
                     <div class="col-6">
                         <h1>Movies</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In doloremque, minima incidunt enim molestias ea earum quidem iusto magnam aut quis illo aliquid vitae repellat? Nam aliquam deleniti expedita consequatur consequuntur eos? Quidem eveniet, atque ipsum vero porro voluptates, inventore fugiat quis officia possimus magnam sit fugit recusandae doloremque veritatis veniam voluptatem quos repellat tempora optio aut id. Voluptatem molestiae aspernatur numquam quas. Voluptatem, tempora. Modi aliquid ut eum perferendis cumque quaerat praesentium magni minima?</p>
                     </div>
                     <div class="col-3 d-flex justify-content-center">
-                        <img class="img-fluid w-75" src="/vite.svg" alt="" />
+                        <img class="img-fluid w-75" src={viteLogo} alt="" />
                     </div>
                 </div>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import preactLogo from '../assets/preact.svg';
+
 export function Navbar() {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
                 <div class="container-fluid">
-                    <img class="img-fluid me-2" src="/preact.svg" alt="" />
+                    <img class="img-fluid me-2" src={preactLogo} alt="" />
                     <a class="navbar-brand" href="/">Navbar</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
